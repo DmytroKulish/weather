bff39331354e80d2074a41d8e52ab85c01b4b32e
DmytroKulish <62489472+DmytroKulish@users.noreply.github.com>
2020-06-22 17:49:21 -0700

    Add files via upload
    Changed structure of AJAX request

--- app.js
+++ app.js
@@ -6,30 +6,32 @@ let flag = document.querySelector("img");
 document.querySelector("button").addEventListener("click", lol);
 document.addEventListener("keypress", (e) => {
   if (e.code === "Enter" || e.charCode === 13) {
-    lol();
+    lol().catch(() => {
+      tempInC = "";
+      condition.textContent = "";
+      flag.src = "";
+      temp.textContent = "";
+      nameOfcity.textContent = "Oopsy, can't find anything";
+    });
   }
 });
-function lol() {
-  let search = document.querySelector("input").value;
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=6b157cd475fb5dc09bd04bb0a27891ec`
-  ).then((response) => {
-    response
-      .json()
-      .then((data) => {
-        tempInC = data.main.temp - 273.15;
-        nameOfcity.textContent = `${data.name}, ${data.sys.country}`;
-        temp.textContent = `${tempInC.toFixed(1)} °C`;
-        condition.textContent = `${data.weather[0].main}`;
-        flag.src = `https://www.countryflags.io/${data.sys.country}/shiny/64.png`;
-        console.log(data);
-      })
-      .catch(() => {
-        tempInC = "";
-        condition.textContent = "";
-        flag.src = "";
-        temp.textContent = "";
-        nameOfcity.textContent = "Oopsy, can't find anything";
-      });
-  });
+async function lol() {
+  try {
+    let search = document.querySelector("input").value;
+    const res = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=6b157cd475fb5dc09bd04bb0a27891ec`
+    );
+    tempInC = res.data.main.temp - 273.15;
+    nameOfcity.textContent = `${res.data.name}, ${res.data.sys.country}`;
+    temp.textContent = `${tempInC.toFixed(1)} °C`;
+    condition.textContent = `${res.data.weather[0].main}`;
+    flag.src = `https://www.countryflags.io/${res.data.sys.country}/shiny/64.png`;
+    console.log(res);
+  } catch (err) {
+    tempInC = "";
+    condition.textContent = "";
+    flag.src = "";
+    temp.textContent = "";
+    nameOfcity.textContent = "Oopsy, can't find anything";
+  }
 }
